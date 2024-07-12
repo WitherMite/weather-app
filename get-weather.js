57be6e4e21208b5a2895eb3eb7cd3d54dffe20d0
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-07-12 00:49:39 -0500

    Change API to Visual Crossing

--- get-weather.js
+++ get-weather.js
@@ -1,18 +1,18 @@
 export default async function getWeather(locationSearch) {
   const request = await requestForecast(locationSearch);
   console.log(request);
-  const current = reduceCurrentWeather(request.current);
-  const location = reduceLocation(request.location);
-  const forecast = request.forecast.forecastday.map(reduceForecastDay);
+
+  const location = request.resolvedAddress;
+  const current = reduceCurrentWeather(request.currentConditions);
+  const forecast = request.days.map(reduceDay);
+
+  // reduce request to needed properties
 
   return { current, forecast, location };
 }
 
 async function requestForecast(location) {
-  const link =
-    "http://api.weatherapi.com/v1/forecast.json?key=2965c6b027c24232bdd203628240707&days=3";
-  const search = "&q=" + location;
-  const url = link + search;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=KCDTTFSSD3KMXDRP3VE6RB66K&include=current`;
 
   try {
     const weather = await fetch(url).then((response) => response.json());
@@ -24,8 +24,6 @@ async function requestForecast(location) {
 
 // TODO: pass through needed properties after designing webpage
 
-function reduceForecastDay(day) {}
+function reduceDay(day) {}
 
-function reduceCurrentWeather(current) {}
-
-function reduceLocation(location) {}
+function reduceCurrentWeather(weather) {}
